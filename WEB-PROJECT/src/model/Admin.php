b49a5bf56dfc7960269e485bbe0a7894a61e817d
Abraham <71876193+abriilo@users.noreply.github.com>
2021-07-01 16:40:53 +0300

    Update Admin.php

--- WEB-PROJECT/src/model/Admin.php
+++ WEB-PROJECT/src/model/Admin.php
@@ -1,4 +1,3 @@
-
 <?php
 
 class Admin{
@@ -16,7 +15,7 @@ class Admin{
         $query = "
         SELECT * FROM ADMIN_ WHERE adminEmail='$email' and adminPassword='$password'
                  ";
-        $handler = self::$connector->getConnection();
+        $handler = self::$admin_db_conn->getConnection();
         $statement = $handler->prepare($query);
         $statement->execute();
 
@@ -49,18 +48,83 @@ class Admin{
     }
 
     public static function createExpert($expert){
-        
+        $email = $expert['email'];
+        $password = $expert['password'];
+        $firstName = $expert['firstName'];
+        $lastName = $expert['lastName'];
+        $phone = $expert['phone'];
+        $address= $expert['address'];
+        $city = $expert['city'];
+        $country = $expert['country'];
+
+        $query = "
+         INSERT INTO EXPERT (expertEmail,exertPassword,expertLastName,expertFirstName, expertPhone,expertAddress,expertCity,expertCountry)
+         VALUES
+         ('$email','$password','$firstName','$lastName','$phone','$address','$city','$country')
+        ";
+        $handler = self::$connector->getConnection();
+        $statement = $handler->prepare($query);
+
+        return $statement->execute();
     }
 
     public static function readExpert($expertId){
         
+        $query = "
+         SELECT * FROM EXPERT
+        ";
+        $handler = self::$connector->getConnection();
+        $statement = $handler->prepare($query);
+        $statement->execute();
+
+        $statement->setFetchMode(PDO::FETCH_ASSOC);
+        $result = $statement->fetchAll();
+
+        $data = ['status' => true];
+        $response = [];
+        foreach($result as $expert){
+
+            $email = $expert['expertEmail'];
+            $password = $expert['expertPassword'];
+            $firstName = $expert['expertFirstName'];
+            $lastName = $expert['ExpertLastName'];
+            $phone = $expert['expertPhone'];
+            $address= $expert['expertAddress'];
+            $city = $expert['expertCity'];
+            $country = $expert['expertCountry'];
+
+         }
+        array_push($data, $response);
+        return $data;
     }
 
-    public static function updateExpert($expertId){
+    public static function updateExpert($expertId, $expert){
         
+        $email = $expert['email'];
+        $password = $expert['password'];
+        $firstName = $expert['firstName'];
+        $lastName = $expert['lastName'];
+        $phone = $expert['phone'];
+        $address= $expert['address'];
+        $city = $expert['city'];
+        $country = $expert['country'];
+
+        $query = "
+         UPDATE EXPERT SET expertEmail='.$email.'expertPassword='.$password.'
+         .'expertFirstName='.$expertFirstName'.'expertLastName='.$expertLastName WHERE expertId='$expertId'
+        ";
+        $handler = self::$connector>getConnection();
+        $statement = $handler->prepare($query);
+
+        return $statement->execute();
     }
 
     public static function deleteExpert($expertId){
         
+        $query = "DELETE FROM EXPERT WHERE expertId=".$expertId;
+        $handler = self::$connector->getConnection();
+        $statement = $handler->prepare($query);
+
+        return $statement->execute();
     }
 }
